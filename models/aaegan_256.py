854d367d507c12965db9f6d0db05f987a01f5269
Greg Johnson <gregj@alleninstitute.org>
2017-05-18 13:29:20 -0700

    extended to be compatable with pt2 fo the integrated cell model

--- models/aaegan_256.py
+++ models/aaegan_256.py
@@ -7,11 +7,15 @@ ksize = 4
 dstep = 2
 
 class Enc(nn.Module):
-    def __init__(self, nlatentdim, insize, nch, gpu_ids, opt=None):
+    def __init__(self, nLatentDim, nClasses, nRef, insize, nch, gpu_ids, opt=None):
         super(Enc, self).__init__()
         
         self.gpu_ids = gpu_ids
         self.fcsize = insize/64
+        
+        self.nLatentDim = nLatentDim
+        self.nClasses = nClasses
+        self.nRef = nRef
         
         self.main = nn.Sequential(
             nn.Conv2d(nch, 64, ksize, dstep, 1),
@@ -40,32 +44,65 @@ class Enc(nn.Module):
             nn.PReLU()
         )
         
-        self.fc = nn.Linear(1024*int(self.fcsize**2), nlatentdim)
-        self.bnEnd = nn.BatchNorm1d(nlatentdim)
+        if self.nClasses > 0:
+            self.classOut = nn.Sequential(
+                nn.Linear(1024*int(self.fcsize**2), self.nClasses),
+                nn.BatchNorm1d(self.nClasses),
+                nn.LogSoftmax()
+            )
         
+        if self.nRef > 0:
+            self.refOut = nn.Sequential(
+                nn.Linear(1024*int(self.fcsize**2), self.nRef),
+                nn.BatchNorm1d(self.nRef)
+            )
+        
+        if self.nLatentDim > 0:
+            self.latentOut = nn.Sequential(
+                nn.Linear(1024*int(self.fcsize**2), self.nLatentDim),
+                nn.BatchNorm1d(self.nLatentDim)
+            )
+            
     def forward(self, x):
         # gpu_ids = None
         # if isinstance(x.data, torch.cuda.FloatTensor) and len(self.gpu_ids) > 1:
         gpu_ids = self.gpu_ids
             
         x = nn.parallel.data_parallel(self.main, x, gpu_ids)
-        
         x = x.view(x.size()[0], 1024*int(self.fcsize**2))
-        x = self.bnEnd(self.fc(x))
-    
-        return x
+        
+        xOut = list()
+                
+        if self.nClasses > 0:
+            xClasses = nn.parallel.data_parallel(self.classOut, x, gpu_ids)
+            xOut.append(xClasses)
+            
+        if self.nRef > 0: 
+            xRef = nn.parallel.data_parallel(self.refOut, x, gpu_ids)
+            xOut.append(xRef)
+        
+        if self.nLatentDim > 0:
+            xLatent = nn.parallel.data_parallel(self.latentOut, x, gpu_ids)
+            xOut.append(xLatent)
+        
+        return xOut
     
 class Dec(nn.Module):
-    def __init__(self, nlatentdim, insize, nch, gpu_ids, opt=None):
+    def __init__(self, nLatentDim, nClasses, nRef, insize, nch, gpu_ids, opt=None):
         super(Dec, self).__init__()
         
         self.gpu_ids = gpu_ids
         self.fcsize = int(insize/64)
         
-        self.fc = nn.Linear(nlatentdim, 1024*int(self.fcsize**2))
+        self.nLatentDim = nLatentDim
+        self.nClasses = nClasses
+        self.nRef = nRef
+        
+        self.fc = nn.Linear(self.nLatentDim + self.nClasses + self.nRef, 1024*int(self.fcsize**2))
+        
         self.main = nn.Sequential(
             # nn.BatchNorm2d(1024),
-        
+            
             nn.PReLU(),
             nn.ConvTranspose2d(1024, 1024, ksize, dstep, 1),
             nn.BatchNorm2d(1024),
@@ -92,10 +129,12 @@ class Dec(nn.Module):
             nn.Sigmoid()             
         )
             
-    def forward(self, x):
+    def forward(self, xIn):
         # gpu_ids = None
         # if isinstance(x.data, torch.cuda.FloatTensor) and len(self.gpu_ids) > 1:
         gpu_ids = self.gpu_ids
+        
+        x = torch.cat(xIn, 1)
         
         x = self.fc(x)
         x = x.view(x.size()[0], 1024, self.fcsize, self.fcsize)
