f1a43494ef034cbf995e6c227e155cb18a530d2e
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2017-06-14 10:09:46 -0700

    aaegan train update

--- train_modules/aaegan_trainv2.py
+++ train_modules/aaegan_trainv2.py
@@ -9,39 +9,39 @@ import pdb
 class trainer(object):
     def __init__(self, dp, opt):
         gpu_id = opt.gpu_ids[0]
-        
+
         self.x = Variable(dp.get_images(range(0, opt.batch_size),'train')).cuda(gpu_id)
-        
+
         if opt.nClasses > 0:
             self.classes = Variable(torch.LongTensor(opt.batch_size)).cuda(gpu_id)
         else:
             self.classes = None
-            
+
         if opt.nRef > 0:
             self.classes = Variable(torch.LongTensor(opt.batch_size, opt.nRef)).cuda(gpu_id)
         else:
             self.ref = None
-        
+
         self.zReal = Variable(torch.FloatTensor(opt.batch_size, opt.nlatentdim)).cuda(gpu_id)
-        
+
         self.y_zReal = Variable(torch.ones(opt.batch_size)).cuda(gpu_id)
         self.y_zFake = Variable(torch.zeros(opt.batch_size)).cuda(gpu_id)
-        
+
         if opt.nClasses > 0:
             self.y_xReal = self.classes
             self.y_xFake = Variable(torch.LongTensor(opt.batch_size)).cuda(gpu_id)
         else:
             self.y_xReal = self.y_zReal
             self.y_xFake = self.y_zFake
-    
-    def iteration(self, enc, dec, encD, decD, 
-                  optEnc, optDec, optEncD, optDecD, 
+
+    def iteration(self, enc, dec, encD, decD,
+                  optEnc, optDec, optEncD, optDecD,
                   critRecon, critZClass, critZRef, critEncD, critDecD,
                   dataProvider, opt):
         gpu_id = opt.gpu_ids[0]
 
-        ###update the discriminator
-        #maximize log(AdvZ(z)) + log(1 - AdvZ(Enc(x)))
+        ### update the discriminator
+        # maximize log(AdvZ(z)) + log(1 - AdvZ(Enc(x)))
         for p in encD.parameters(): # reset requires_grad
             p.requires_grad = True # they are set to False below in netG update
 
@@ -56,10 +56,10 @@ class trainer(object):
 
         rand_inds_encD = np.random.permutation(opt.ndat)
         inds = rand_inds_encD[0:opt.batch_size]
-        
+
         self.x.data.copy_(dataProvider.get_images(inds,'train'))
         x = self.x
-        
+
         if opt.nClasses > 0:
             self.classes.data.copy_(dataProvider.get_classes(inds,'train'))
             x = self.classes
@@ -67,9 +67,9 @@ class trainer(object):
         if opt.nRef > 0:
             self.ref.data.copy_(dataProvider.get_ref(inds,'train'))
             ref = self.ref
-            
+
         zAll = enc(x)
-            
+
         for var in zAll:
             var.detach_()
 
@@ -77,7 +77,6 @@ class trainer(object):
 
         self.zReal.data.normal_()
         zReal = self.zReal
-        # zReal = Variable(opt.latentSample(opt.batch_size, opt.nlatentdim).cuda(gpu_id))
         zFake = zAll[-1]
 
         optEnc.zero_grad()
@@ -92,18 +91,16 @@ class trainer(object):
         # train with real
         yHat_zReal = encD(zReal)
         errEncD_real = critEncD(yHat_zReal, y_zReal)
-        errEncD_real.backward(retain_graph=True)
 
         # train with fake
         yHat_zFake = encD(zFake)
         errEncD_fake = critEncD(yHat_zFake, y_zFake)
-        errEncD_fake.backward(retain_graph=True)
 
         encDLoss = (errEncD_real + errEncD_fake)/2
-
+        encDLoss.backward(retain_graph=True)
         optEncD.step()
-        
-        ###Train decD 
+
+        ### Train decD
         if opt.nClasses > 0:
             y_xReal = classes
             self.y_xFake.data.fill_(opt.nClasses)
@@ -111,22 +108,19 @@ class trainer(object):
         else:
             y_xReal = self.y_xReal
             y_xFake = self.y_xFake
-            
+
         yHat_xReal = decD(x)
         errDecD_real = critDecD(yHat_xReal, y_xReal)
-        # errDecD_real.backward(retain_graph=True)
 
-        #train with fake, reconstructed
-        yHat_xFake = decD(xHat)        
+        # train with fake, reconstructed
+        yHat_xFake = decD(xHat)
         errDecD_fake = critDecD(yHat_xFake, y_xFake)
-        # errDecD_fake.backward(retain_graph=True)
 
-        #train with fake, sampled and decoded
+        # train with fake, sampled and decoded
         zAll[-1] = zReal
 
         yHat_xFake2 = decD(dec(zAll))
         errEncD_fake2 = critDecD(yHat_xFake2, y_xFake)
-        # errEncD_fake2.backward(retain_graph=True)
 
         decDLoss = (errDecD_real + (errDecD_fake + errEncD_fake2)/2)/2
         decDLoss.backward(retain_graph=True)
@@ -135,9 +129,6 @@ class trainer(object):
         encDLoss = encDLoss.data[0]
         decDLoss = decDLoss.data[0]
 
-#         yHat_zReal = None
-#         yHat_zFake = None
-
         errEncD_real = None
         errEncD_fake = None
 
@@ -145,15 +136,12 @@ class trainer(object):
         errDecD_fake = None
         errEncD_fake2 = None
 
-#         yHat_xReal = None
-#         yHat_xFake = None
-#         yHat_xFake2 = None
 
         for p in enc.parameters():
             p.requires_grad = True
 
         for p in dec.parameters():
-            p.requires_grad = True  
+            p.requires_grad = True
 
         for p in encD.parameters():
             p.requires_grad = False
@@ -163,32 +151,34 @@ class trainer(object):
 
         optEnc.zero_grad()
         optDec.zero_grad()
-        optEncD.zero_grad()  
-        optDecD.zero_grad()          
+        optEncD.zero_grad()
+        optDecD.zero_grad()
 
         ## train the autoencoder
         zAll = enc(x)
-        xHat = dec(zAll)    
+        xHat = dec(zAll)
 
-        c = 0        
+        c = 0
         if opt.nClasses > 0:
             classLoss = critZClass(zAll[c], classes)
-            classLoss.backward(retain_graph=True)        
             c += 1
+        else:
+            classLoss = Variable(torch.zeros(1)).cuda(gpu_id)
 
         if opt.nRef > 0:
             refLoss = critZRef(zAll[c], ref)
-            refLoss.backward(retain_graph=True)        
             c += 1
+        else:
+            refLoss = Variable(torch.zeros(1)).cuda(gpu_id)
 
         reconLoss = critRecon(xHat, x)
-        reconLoss.backward(retain_graph=True)        
 
-        #update wrt encD
+        # update wrt encD
         yHatFake = encD(zAll[c])
         minimaxEncDLoss = critEncD(yHatFake, y_zReal)
-        (minimaxEncDLoss.mul(opt.encDRatio)).backward(retain_graph=True)
 
+        totEncLoss = classLoss + refLoss + reconLoss + minimaxEncDLoss.mul(opt.encDRatio)
+        totEncLoss.backward(retain_graph=True)
         optEnc.step()
 
         minimaxEncDLoss = minimaxEncDLoss.data[0]
@@ -196,30 +186,26 @@ class trainer(object):
         for p in enc.parameters():
             p.requires_grad = False
 
-        #update wrt decD(dec(enc(X)))
+        # update wrt decD(dec(enc(X)))
         yHat_xFake = decD(xHat)
         minimaxDecDLoss = critDecD(yHat_xFake, y_xReal)
-        (minimaxDecDLoss.mul(opt.decDRatio).div(2)).backward(retain_graph=True)
-        minimaxDecDLoss = minimaxDecDLoss.data[0]
+        minimaxDecDLoss_for_logger = minimaxDecDLoss.data[0]
         yHat_xFake = None
-        
-        #update wrt decD(dec(Z))
+
+        # update wrt decD(dec(Z))
         self.zReal.data.normal_()
         zAll[c] = self.zReal
-        # zAll[c] = Variable(opt.latentSample(opt.batch_size, opt.nlatentdim).cuda(gpu_id))
         xHat = dec(zAll)
 
         yHat_xFake2 = decD(xHat)
         minimaxDecDLoss2 = critDecD(yHat_xFake2, y_xReal)
-        (minimaxDecDLoss2.mul(opt.decDRatio).div(2)).backward(retain_graph=True)
-        minimaxDecDLoss2 = minimaxDecDLoss2.data[0]
+        minimaxDecDLoss2_for_logger = minimaxDecDLoss2.data[0]
         yHat_xFake2 = None
-        
-        minimaxDecLoss = (minimaxDecDLoss+minimaxDecDLoss2)/2
-        
+
+        totalDecDLoss = (minimaxDecDLoss+minimaxDecDLoss2)/2
+        totalDecDLoss.backward(retain_graph=True)
         optDec.step()
-        
-        
+
 
         errors = (reconLoss.data[0],)
         if opt.nClasses > 0:
@@ -228,7 +214,6 @@ class trainer(object):
         if opt.nRef > 0:
             errors += (refLoss.data[0],)
 
-        errors += (minimaxEncDLoss, encDLoss, minimaxDecLoss, decDLoss)
+        errors += (minimaxEncDLoss, encDLoss, minimaxDecDLoss_for_logger, decDLoss)
 
         return errors, zFake.data
-    
