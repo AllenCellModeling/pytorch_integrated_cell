5a4608ea405a74b38426822d3dc8f7483b2b43c6
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2017-09-26 15:47:46 -0700

    modular start script

--- start_pytorch_docker_jupyter.sh
+++ start_pytorch_docker_jupyter.sh
@@ -1,8 +1,16 @@
+#!/usr/bin/env bash
+
+PORT=${1}
+PROJ_DIR=$(dirname $(pwd -P))
+DATA_DIR='/allen/aics/modeling/data'
+RSLT_DIR='/allen/aics/modeling/gregj/results'
+
 nvidia-docker run -it \
-	-e "PASSWORD=jupyter1" \
-	-p 9602:9999 \
-	-v /allen/aics/modeling/rorydm/pytorch_integrated_cell:/root/projects \
-	-v /allen/aics/modeling/data:/root/data \
-	gregj/pytorch_extended \
-	bash -c "jupyter notebook --allow-root --NotebookApp.iopub_data_rate_limit=10000000000"
+    -e "PASSWORD=jupyter1" \
+    -p ${PORT}:9999 \
+    -v ${PROJ_DIR}:/root/projects \
+    -v ${DATA_DIR}:/root/data \
+    -v ${RSLT_DIR}:/root/results \
+    rorydm/pytorch_extended:pytorch0.2_conda \
+    bash -c "jupyter notebook --allow-root --NotebookApp.iopub_data_rate_limit=10000000000"
 
