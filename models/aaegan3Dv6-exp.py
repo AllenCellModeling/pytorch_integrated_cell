fixed the way variables were broadcast
Former-commit-id: 0aa2b0403c6fe0a30218e32a1dbf86b1a34538c8

models/aaegan3Dv6-exp.py
@@ -134,10 +134,11 @@ class Dec(nn.Module):
         # if isinstance(x.data, torch.cuda.FloatTensor) and len(self.gpu_ids) > 1:
         gpu_ids = self.gpu_ids
         
+        xInExp = xIn[0]        
         if self.nClasses > 0:
-            xIn[0] = torch.exp(xIn[0])
-        
-        x = torch.cat(xIn, 1)
+            xInExp = torch.exp(xIn[0])
+
+        x = torch.cat([xInExp] + xIn[1:], 1)
         
         x = self.fc(x)
         x = x.view(x.size()[0], 1024, self.fcsize, 1, 1)