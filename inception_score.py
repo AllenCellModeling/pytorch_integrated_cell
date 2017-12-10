85aff9998b89fb72e00d8ab93e95ea8695c7c63f
Gregory Johnson <gregj@alleninstitute.org>
2017-12-09 20:06:33 -0800

    Batch-processing of inception score. Does data, autoencoded data, and generated data
    Former-commit-id: 9ff27fa0b2d7d91f225c4241dab1b48ef7a6af20

--- inception_score.py
+++ inception_score.py
@@ -39,12 +39,14 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--parent_dir', help='save dir')
 parser.add_argument('--gpu_ids', nargs='+', type=int, default=[0], help='gpu id')
+parser.add_argument('--batch_size', type=int, default=200, help='gpu id')
+parser.add_argument('--overwrite', type=bool, default=False, help='overwrite?')
 args = parser.parse_args()
 
 model_dir = args.parent_dir + os.sep + 'struct_model'
 ref_dir = args.parent_dir + os.sep + 'ref_model'
 
-save_dir = args.parent_dir + os.sep + 'analysis' + os.sep + 'inception_scare' + os.sep
+save_dir = args.parent_dir + os.sep + 'analysis' + os.sep + 'inception_score' + os.sep
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
         
@@ -74,6 +76,8 @@ opt.nRef = opt.nlatentdim
 
 models, _, _, _, opt = model_utils.load_model(opt.model_name, opt)
 
+opt.batch_size = args.batch_size
+
 enc = models['enc']
 dec = models['dec']
 enc.train(False)
@@ -99,84 +103,135 @@ import pandas as pd
 
 
 
-########
-# data #
-########
+##########
+## data ##
+##########
+fname = os.path.join(save_dir,'im_class_log_probs_data.pickle')
 
-# im_class_log_probs_data = {}
+if os.path.exists(fname) and not args.overwrite:
+    pass
+else:
 
-# # For train or test
-# for train_or_test in ['test', 'train']:
-#     ndat = dp.get_n_dat(train_or_test)
-#     pred_log_probs = np.zeros([ndat,opt.nClasses])
-   
-#     # For each cell in the data split
-#     for i in tqdm(range(0, 1)):
-#     #for i in tqdm(range(0, ndat)):
+    im_class_log_probs = {}
 
-#         img_index = dp.data[train_or_test]['inds'][i]
- 
-#         # Load the image
-#         img_in = dp.get_images([i], train_or_test)
-#         img_in = Variable(img_in.cuda(gpu_id), volatile=True)
+    # For train or test
+    for train_or_test in ['test', 'train']:
+        ndat = dp.get_n_dat(train_or_test)
+        inds = np.arange(0, ndat)    
 
-#         # pass forward through the model
-#         z = enc(img_in)
-#         print(z)
-#         for w in z:
-#             print(z.shape)
-#             print(w.data)
-        
-# #         p = z[0].data.cpu().numpy()
-# #         pred_log_probs[i,:] = p
+        pred_log_probs = np.zeros([ndat,opt.nClasses])
 
-# #    im_class_log_probs_data[train_or_test] = pred_log_probs
+        iter_struct = [inds[j:j+opt.batch_size] for j in range(0, len(inds), opt.batch_size)] 
 
-# # save test and train preds
-# # fname = os.path.join(save_dir,'im_class_log_probs_data.pickle')
-# # with open(fname, 'wb') as handle:
-# #     pickle.dump(im_class_log_probs_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        # For each cell in the data split
+    #     for i in tqdm(range(0, 1)):
+        for i in tqdm(iter_struct, desc='data, ' + train_or_test):
+
+            # Load the image
+            img_in = dp.get_images(i, train_or_test)
+            img_in = Variable(img_in.cuda(gpu_id), volatile=True)
+
+            # pass forward through the model
+            z = enc(img_in)
+            
+            p = z[0].data.cpu().numpy()
+            pred_log_probs[i,:] = p
+
+        im_class_log_probs[train_or_test] = pred_log_probs
+
+    # save test and train preds
+
+    with open(fname, 'wb') as handle:
+        pickle.dump(im_class_log_probs, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 #############
-# generated #
+# autoencoded #
 #############
 
-im_class_log_probs_gen = {}
+fname = os.path.join(save_dir,'im_class_log_probs_autoencode.pickle')
 
-# For train or test
-for train_or_test in ['test', 'train']:
-    ndat = dp.get_n_dat(train_or_test)
-    pred_log_probs = np.zeros([ndat,opt.nClasses])
+if os.path.exists(fname) and not args.overwrite:
+    pass
+else:
 
-    # For each cell in the data split
-    for i in tqdm(range(0, ndat)):
+    im_class_log_probs = {}
 
-        # for each image in the real data set
-        img_index = dp.data[train_or_test]['inds'][i]
-        
-        # get the integer label of its class
-        img_class = dp.get_classes([i], train_or_test, index_or_onehot = 'index')[0]
+    # For train or test
+    for train_or_test in ['test', 'train']:
+        ndat = dp.get_n_dat(train_or_test)
+        inds = np.arange(0, ndat)      
 
-        # create the log onehot class vector
-        classes = Variable(torch.Tensor(ndat, opt.nClasses).fill_(-25).cuda(gpu_id), volatile=True)
-        classes[img_class] = 0
+        pred_log_probs = np.zeros([ndat,opt.nClasses])
 
-        # sample random latent space vectors
-        ref = Variable(torch.Tensor(ndat, opt.nRef).normal_().cuda(gpu_id), volatile=True)
-        struct = Variable(torch.Tensor(ndat, opt.nRef).normal_().cuda(gpu_id), volatile=True)
+        iter_struct = [inds[j:j+opt.batch_size] for j in range(0, len(inds), opt.batch_size)] 
 
-        # generate a fake cell of corresponding class
-        img_in = dec([classes, ref, struct])
+        for i in tqdm(iter_struct, desc='autoencode, ' + train_or_test):
 
-        # pass forward through the model
-        z = enc(img_in)
-        p = z[0].data.cpu().numpy()
-        pred_log_probs[i,:] = p
+            # Load the image
+            img_in = dp.get_images(i, train_or_test)
+            img_in = Variable(img_in.cuda(gpu_id), volatile=True)
 
-    im_class_log_probs_gen[train_or_test] = pred_log_probs
+            # pass forward through the model
+            z = enc(dec(enc(img_in)))
 
-# save test and train preds
+            p = z[0].data.cpu().numpy()
+            pred_log_probs[i,:] = p
+
+        im_class_log_probs[train_or_test] = pred_log_probs
+
+    # save test and train preds
+
+    with open(fname, 'wb') as handle:
+        pickle.dump(im_class_log_probs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+
+#############
+# autoencoded #
+#############
 fname = os.path.join(save_dir,'im_class_log_probs_gen.pickle')
-with open(fname, 'wb') as handle:
-    pickle.dump(im_class_log_probs_gen, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+if os.path.exists(fname) and not args.overwrite:
+    pass
+else:
+
+    im_class_log_probs = {}
+
+    # For train or test
+    for class_id in range(0, dp.get_n_classes()):
+    #     ndat = dp.get_n_dat(train_or_test)
+        ndat = 2000
+        inds = np.arange(0, ndat)    
+
+        pred_log_probs = np.zeros([ndat,opt.nClasses])
+
+        iter_struct = [inds[j:j+opt.batch_size] for j in range(0, len(inds), opt.batch_size)] 
+
+        # For each cell in the data split
+        for i in tqdm(iter_struct, desc='gen, ' + str(class_id+1) + os.sep + str(dp.get_n_classes()) ):
+
+            # create the log onehot class vector
+            classes = Variable(torch.Tensor(ndat, opt.nClasses).fill_(-25).cuda(gpu_id), volatile=True)
+            classes[:, class_id] = 0
+
+            # sample random latent space vectors
+            ref = Variable(torch.Tensor(ndat, opt.nRef).normal_().cuda(gpu_id), volatile=True)
+            struct = Variable(torch.Tensor(ndat, opt.nRef).normal_().cuda(gpu_id), volatile=True)
+
+            # generate a fake cell of corresponding class
+            img_in = dec([classes, ref, struct])
+
+            # pass forward through the model
+            z = enc(img_in)
+            p = z[0].data.cpu().numpy()
+            pred_log_probs[i,:] = p
+
+        im_class_log_probs[train_or_test] = pred_log_probs
+
+    # save test and train preds
+
+    with open(fname, 'wb') as handle:
+        pickle.dump(im_class_log_probs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+    
