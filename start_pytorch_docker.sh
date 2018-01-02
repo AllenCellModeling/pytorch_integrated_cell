6ae1c10f977745afffc9875f43dfb02d201a5a63
Gregory Johnson <gregj@alleninstitute.org>
2018-01-02 11:37:04 -0800

    added script to start docker image

--- start_pytorch_docker.sh
+++ start_pytorch_docker.sh
@@ -1,4 +1,6 @@
 nvidia-docker run -it \
-	-v /allen/aics/modeling/rorydm/pytorch_integrated_cell:/root/projects \
-	-v /allen/aics/modeling/data:/root/data \
-	gregj/pytorch_extended bash
+	-v /allen/aics/modeling/gregj/projects:/root/projects \
+        -v /allen/aics/modeling/gregj/results:/root/results \
+        -v /raid/gregj/cache:/root/data \
+	gregj/pytorch_extras:dgx_jupyter \
+	bash 
