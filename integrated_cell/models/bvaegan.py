89971afd1c3c66d8caf4ffb48a4fdf885d93b521
Gregory Johnson <gregj@alleninstitute.org>
2018-07-06 14:24:32 -0700

    VAE, BVAE, and BVAEGAN training code
    Former-commit-id: 9c0eecb7efd88237d1a23a95744e5da33639636e

--- integrated_cell/models/bvaegan.py
+++ integrated_cell/models/bvaegan.py
@@ -9,36 +9,10 @@ import pdb
 from integrated_cell.model_utils import *
 from integrated_cell.utils import plots as plots
 
-from integrated_cell.models import base_model 
-
 # This is the trainer for the Beta-VAE
 
-def reparameterize(mu, log_var, add_noise = True):
-    if add_noise:
-        std = log_var.div(2).exp()
-        eps = torch.randn_like(std)
-        out = eps.mul(std).add_(mu)
-    else:
-        out = mu
-        
-    return out
-
-def kl_divergence(mu, logvar):
-    batch_size = mu.size(0)
-    assert batch_size != 0
-    if mu.data.ndimension() == 4:
-        mu = mu.view(mu.size(0), mu.size(1))
-        
-    if logvar.data.ndimension() == 4:
-        logvar = logvar.view(logvar.size(0), logvar.size(1))
-
-    klds = -0.5*(1 + logvar - mu.pow(2) - logvar.exp())
-    total_kld = klds.sum(1).mean(0, True)
-    dimension_wise_kld = klds.mean(0)
-    mean_kld = klds.mean(1).mean(0, True)
-
-    return total_kld, dimension_wise_kld, mean_kld
-
+from integrated_cell.models import bvae
+from integrated_cell.models import base_model 
 
 
 class Model(base_model.Model):
@@ -47,7 +21,6 @@ class Model(base_model.Model):
         
         if beta_step is None:
             self.beta = beta
-            self.beta_step = None
         else:
             self.beta = 0
             self.beta_step = beta_step
@@ -55,30 +28,120 @@ class Model(base_model.Model):
             
         self.objective = objective
 
+
     def iteration(self,
-                  enc, dec,
-                  optEnc, optDec,
-                  critRecon, critZClass, critZRef,
+                  enc, dec, decD,
+                  optEnc, optDec, optDecD,
+                  critRecon, critZClass, critZRef, critDecD,
                   data_provider, opt):
         gpu_id = self.gpu_ids[0]
-        
-        
-    
+
+        #do this just incase anything upstream changes these values
+        enc.train(True)
+        dec.train(True)
+        decD.train(True)
+
+        ###update the discriminator
+        #maximize log(AdvZ(z)) + log(1 - AdvZ(Enc(x)))
+
         rand_inds_encD = np.random.permutation(opt.ndat)
         inds = rand_inds_encD[0:self.batch_size]
 
         self.x.data.copy_(data_provider.get_images(inds,'train'))
         x = self.x
 
+        y_xFake = self.y_xFake
+        y_zReal = self.y_zReal
+
+        if self.n_classes == 0:
+            y_xReal = self.y_xReal
+            y_zFake = self.y_zFake
+        else:
+            self.classes.data.copy_(data_provider.get_classes(inds,'train'))
+            classes = self.classes
+
+            y_xReal = classes
+            y_zFake = classes
+
+        if self.n_ref > 0:
+            self.ref.data.copy_(data_provider.get_ref(inds,'train'))
+            ref = self.ref
+
+        for p in decD.parameters():
+            p.requires_grad = True
+
+        for p in enc.parameters():
+            p.requires_grad = False
+
+        for p in dec.parameters():
+            p.requires_grad = False
+
+        zAll = enc(x)
+
+        for i in range(len(zAll)-1):
+                zAll[i].detach_()
+
+        for var in zAll[-1]:
+            var.detach_()
+
+        zAll[-1] = bvae.reparameterize(zAll[-1][0], zAll[-1][1])
+
+        xHat = dec(zAll)
+
+        self.zReal.data.normal_()
+        zReal = self.zReal
+        # zReal = Variable(opt.latentSample(self.batch_size, self.n_latent_dim).cuda(gpu_id))
+        zFake = zAll[-1]
+
+        optEnc.zero_grad()
+        optDec.zero_grad()
+        optDecD.zero_grad()
+
+        ##############
+        ### Train decD
+        ##############
+
+        yHat_xReal = decD(x)
+
+        ### train with real
+        errDecD_real = critDecD(yHat_xReal, y_xReal)
+
+        ### train with fake, reconstructed
+        yHat_xFake = decD(xHat)
+        errDecD_fake = critDecD(yHat_xFake, y_xFake)
+
+        ### train with fake, sampled and decoded
+        zAll[-1] = zReal
+
+        yHat_xFake2 = decD(dec(zAll))
+        errDecD_fake2 = critDecD(yHat_xFake2, y_xFake)
+
+        decDLoss = (errDecD_real + (errDecD_fake + errDecD_fake2)/2)/2
+        decDLoss.backward(retain_graph=True)
+        optDecD.step()
+
+        decDLoss = decDLoss.data[0]
+
+        errDecD_real = None
+        errDecD_fake = None
+        errDecD_fake2 = None
+
+        for p in enc.parameters():
+            p.requires_grad = True
+
+        for p in dec.parameters():
+            p.requires_grad = True
+
+        for p in decD.parameters():
+            p.requires_grad = False
+
+        optEnc.zero_grad()
+        optDec.zero_grad()
+        optDecD.zero_grad()
+
         #####################
         ### train autoencoder
         #####################
-
-        enc.train(True)
-        dec.train(True)
-        
-        optEnc.zero_grad()
-        optDec.zero_grad()
 
         ### Forward passes
         zAll = enc(x)
@@ -98,31 +161,73 @@ class Model(base_model.Model):
             refLoss = refLoss.data[0]
             c += 1
 
-        total_kld, dimension_wise_kld, mean_kld = kl_divergence(zAll[c][0], zAll[c][1])
-        
 
-        zLatent = zAll[c][0]
-        zAll[c] = reparameterize(zAll[c][0], zAll[c][1])
+        total_kld, dimension_wise_kld, mean_kld = bvae.kl_divergence(zAll[c][0], zAll[c][1])
+        kld_loss = total_kld.data[0]
+        minimaxEncDLoss = 0
+
+        zAll[c] = bvae.reparameterize(zAll[c][0], zAll[c][1])
 
         xHat = dec(zAll)
 
         ### Update the image reconstruction
         reconLoss = critRecon(xHat, x)
 
-        if self.beta == 0:
-            beta_vae_loss = reconLoss
-        elif self.objective == 'H':
+        if self.objective == 'H':
             beta_vae_loss = reconLoss + self.beta*total_kld
         elif self.objective == 'B':
             C = torch.clamp(self.C_max/self.C_stop_iter*self.global_iter, 0, self.C_max.data[0])
             beta_vae_loss = recon_loss + self.gamma*(total_kld-C).abs()
 
-        beta_vae_loss.backward()
-        
-        kld_loss = mean_kld.data[0]
+
+        beta_vae_loss.backward(retain_graph=True)
         reconLoss = reconLoss.data[0]
 
         optEnc.step()
+
+        for p in enc.parameters():
+            p.requires_grad = False
+
+        ### update wrt decD(dec(enc(X)))
+        yHat_xFake = decD(xHat)
+        minimaxDecDLoss = critDecD(yHat_xFake, y_xReal)
+        (minimaxDecDLoss.mul(opt.lambdaDecD).div(2)).backward(retain_graph=True)
+        minimaxDecDLoss = minimaxDecDLoss.data[0]
+        yHat_xFake = None
+
+        ### update wrt decD(dec(Z))
+
+        c = 0
+        #if we have classes, create random classes, generate images of random classes
+        if self.n_classes > 0:
+            shuffle_inds = np.arange(0, zAll[0].size(0))
+
+            classes_one_hot = Variable((data_provider.get_classes(inds,'train', 'one hot') - 1) * 25).type_as(zAll[c].data).cuda(self.gpu_ids[0])
+
+            np.random.shuffle(shuffle_inds)
+            zAll[c] = classes_one_hot[shuffle_inds,:]
+            y_xReal = y_xReal[torch.LongTensor(shuffle_inds).cuda(self.gpu_ids[0])]
+
+            c +=1
+
+        if self.n_ref > 0:
+            zAll[c].data.normal_()
+
+
+        #sample random positions in the localization space
+        self.zReal.data.normal_()
+        zAll[-1] = self.zReal
+
+        xHat = dec(zAll)
+
+        yHat_xFake2 = decD(xHat)
+        minimaxDecDLoss2 = critDecD(yHat_xFake2, y_xReal)
+        (minimaxDecDLoss2.mul(opt.lambdaDecD).div(2)).backward(retain_graph=True)
+        minimaxDecDLoss2 = minimaxDecDLoss2.data[0]
+        yHat_xFake2 = None
+
+        minimaxDecLoss = (minimaxDecDLoss+minimaxDecDLoss2)/2
+
         optDec.step()
 
         errors = (reconLoss,)
@@ -132,15 +237,16 @@ class Model(base_model.Model):
         if self.n_ref > 0:
             errors += (refLoss,)
 
-        errors += (kld_loss,)
+        errors += (kld_loss, minimaxDecLoss, decDLoss)
+
         
         if self.beta_step is not None:
             self.beta += self.beta_step
             
             if self.beta > self.beta_max:
                 self.beta = self.beta_max
-
-        return errors, zLatent.data
+        
+        return errors, zFake.data
 
 
     def load(self, model_name, opt):
@@ -149,22 +255,27 @@ class Model(base_model.Model):
 
         enc = model_provider.Enc(self.n_latent_dim, self.n_classes, self.n_ref, self.n_channels, self.gpu_ids, **opt.kwargs_enc)
         dec = model_provider.Dec(self.n_latent_dim, self.n_classes, self.n_ref, self.n_channels, self.gpu_ids, **opt.kwargs_dec)
-       
+        decD = model_provider.DecD(self.n_classes+1, self.n_channels, self.gpu_ids, **opt.kwargs_decD)
+
         enc.apply(weights_init)
         dec.apply(weights_init)
-       
+        decD.apply(weights_init)
+
         gpu_id = self.gpu_ids[0]
 
         enc.cuda(gpu_id)
         dec.cuda(gpu_id)
-       
+        decD.cuda(gpu_id)
+
         if opt.optimizer == 'RMSprop':
             optEnc = optim.RMSprop(enc.parameters(), lr=opt.lrEnc)
             optDec = optim.RMSprop(dec.parameters(), lr=opt.lrDec)
+            optDecD = optim.RMSprop(decD.parameters(), lr=opt.lrDecD)
         elif opt.optimizer == 'adam':
 
             optEnc = optim.Adam(enc.parameters(), lr=opt.lrEnc, **opt.kwargs_optim)
             optDec = optim.Adam(dec.parameters(), lr=opt.lrDec, **opt.kwargs_optim)
+            optDecD = optim.Adam(decD.parameters(), lr=opt.lrDecD, **opt.kwargs_optim)
 
         columns = ('epoch', 'iter', 'reconLoss',)
         print_str = '[%d][%d] reconLoss: %.6f'
@@ -177,8 +288,8 @@ class Model(base_model.Model):
             columns += ('refLoss',)
             print_str += ' refLoss: %.6f'
 
-        columns += ('kldLoss', 'time')
-        print_str += ' kld: %.6f time: %.2f'
+        columns += ('kldLoss', 'minimaxDecDLoss', 'decDLoss', 'time')
+        print_str += ' kld: %.6f mmDecD: %.6f decD: %.6f time: %.2f'
 
         logger = SimpleLogger(columns,  print_str)
 
@@ -187,24 +298,30 @@ class Model(base_model.Model):
 
             load_state(enc, optEnc, '{0}/enc.pth'.format(opt.save_dir), gpu_id)
             load_state(dec, optDec, '{0}/dec.pth'.format(opt.save_dir), gpu_id)
+            load_state(decD, optDecD, '{0}/decD.pth'.format(opt.save_dir), gpu_id)
 
             logger = pickle.load(open( '{0}/logger.pkl'.format(opt.save_dir), "rb" ))
 
             this_epoch = max(logger.log['epoch']) + 1
             iteration = max(logger.log['iter'])
 
-        models = dict()
-        models['enc'] = enc
-        models['dec'] = dec
+        models = {'enc': enc, 'dec': dec, 'decD': decD}
 
         optimizers = dict()
         optimizers['optEnc'] = optEnc
         optimizers['optDec'] = optDec
+        optimizers['optDecD'] = optDecD
 
         criterions = dict()
         criterions['critRecon'] = eval('nn.' + opt.critRecon + '(size_average=False)')
         criterions['critZClass'] = nn.NLLLoss()
         criterions['critZRef'] = nn.MSELoss()
+
+
+        if self.n_classes > 0:
+            criterions['critDecD'] = nn.CrossEntropyLoss()
+        else:
+            criterions['critDecD'] = nn.BCEWithLogitsLoss()
 
         if opt.latentDistribution == 'uniform':
             from integrated_cell.model_utils import sampleUniform as latentSample
@@ -221,8 +338,8 @@ class Model(base_model.Model):
         
         return models, optimizers, criterions, logger
 
-    def save(self, enc, dec,
-                   optEnc, optDec,
+    def save(self, enc, dec, decD,
+                   optEnc, optDec, optDecD,
                    logger, zAll, opt):
     #         for saving and loading see:
     #         https://discuss.pytorch.org/t/how-to-save-load-torch-models/718
@@ -231,6 +348,7 @@ class Model(base_model.Model):
 
         save_state(enc, optEnc, '{0}/enc.pth'.format(opt.save_dir), gpu_id)
         save_state(dec, optDec, '{0}/dec.pth'.format(opt.save_dir), gpu_id)
+        save_state(decD, optDecD, '{0}/decD.pth'.format(opt.save_dir), gpu_id)
 
         pickle.dump(zAll, open('{0}/embedding.pkl'.format(opt.save_dir), 'wb'))
         pickle.dump(logger, open('{0}/logger.pkl'.format(opt.save_dir), 'wb'))
