e9cdf2f68ce6482fed79e01e37d233fac3204596
Gregory Johnson <gregj@alleninstitute.org>
2018-08-01 14:22:04 -0700

    scripts for reproducing results
    Former-commit-id: f8dc04e936329ddfa8427f4df96fe725ce007137

--- scripts/aaegan_short.sh
+++ scripts/aaegan_short.sh
@@ -1,14 +1,15 @@
 cd ..
 python /root/projects/pytorch_integrated_cell/train_model.py \
 	--gpu_ids $1 \
-	--save_dir ./results/aaegan_short_test \
+	--save_dir ./results/aaegan_short \
+        --data_save_path ./results/data.pyt \
 	--lrEnc 2E-4 --lrDec 2E-4 \
 	--lrEncD 2E-2 --lrDecD 1E-4 \
 	--lambdaEncD 1E-3 --lambdaDecD 1E-3 \
 	--model_name aaegan3Dv6-relu-exp \
 	--train_module aaegan_trainv7 \
 	--kwargs_encD '{"noise_std": 0}' \
-    --kwargs_decD '{"noise_std": 2E-1}' \
+        --kwargs_decD '{"noise_std": 2E-1}' \
 	--kwargs_optim '{"betas": [0, 0.9]}' \
 	--imdir /root/results/ipp/ipp_17_10_25 \
 	--dataProvider DataProvider3Dh5 \
