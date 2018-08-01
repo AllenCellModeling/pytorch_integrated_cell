d78d8f89a96964853001aff10fd88bed4ea9d917
Gregory Johnson <gregj@alleninstitute.org>
2018-08-01 14:23:09 -0700

    ignore if a Conv model somehow doesnt have weights associated with it
    Former-commit-id: 2159a6b55a7ba50f3ed8844d2e8ebeb34ff945f1

--- integrated_cell/model_utils.py
+++ integrated_cell/model_utils.py
@@ -74,7 +74,10 @@ def tensor2img(img):
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        m.weight.data.normal_(0.0, 0.02)
+        try:
+            m.weight.data.normal_(0.0, 0.02)
+        except:
+            pass
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
