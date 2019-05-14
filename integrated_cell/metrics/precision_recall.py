d08ef3f75c25784d7d05550d1f12b6b91371163a
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-05-14 11:17:26 -0700

    use pytorch 1.1 named tuples correctly

--- integrated_cell/metrics/precision_recall.py
+++ integrated_cell/metrics/precision_recall.py
@@ -7,19 +7,22 @@ https://arxiv.org/abs/1904.06991
 
 
 def manifold_estimate(phi_a, phi_b, k=3):
-    """phi_a and phi_b are N_a x D and N_b x D sets of features, and
-    k is the nearest neighbor setting for manifold esitmation."""
-    daa = torch.cdist(phi_a, phi_a)
-    r_a, _ = torch.kthvalue(daa, k + 1, dim=0)
-    del daa
-    dab = torch.cdist(phi_a, phi_b)
-    out, _ = torch.max(dab < r_a.view(-1, 1), dim=0)
-    return out.float().mean().item()
+    """
+    phi_a and phi_b are N_a x D and N_b x D sets of features, and
+    k is the nearest neighbor setting for knn manifold estimation.
+    """
+    r_a = torch.kthvalue(torch.cdist(phi_a, phi_a), k + 1, dim=0)
+    d_ab = torch.cdist(phi_a, phi_b)
+    out = torch.max(d_ab < r_a.values.view(-1, 1), dim=0)
+    return out.values.float().mean().item()
 
 
 def precision_recall(phi_r, phi_g, k=3):
-    """phi_r and phi_s are N_a x D and N_b x D sets of features
-    from real and generated images respectively."""
+    """
+    phi_r and phi_s are N_a x D and N_b x D sets of features
+    from real and generated images respectively, and k is the nearest
+    neighbor setting for knn manifold estimation.
+    """
     return {
         "precision": manifold_estimate(phi_r, phi_g, k=k),
         "recall": manifold_estimate(phi_g, phi_r, k=k),
