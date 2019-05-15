ed5b378d2fa86b8750f9ced51fe1d1631394b1da
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-05-15 10:39:41 -0700

    use arbitrary batch size

--- integrated_cell/metrics/precision_recall.py
+++ integrated_cell/metrics/precision_recall.py
@@ -6,35 +6,35 @@ https://arxiv.org/abs/1904.06991
 """
 
 
-def manifold_estimate(phi_a, phi_b, k=3, parallel=True):
+def manifold_estimate(phi_a, phi_b, k=3, batch_size=None):
     """
-    phi_a and phi_b are N_a x D and N_b x D sets of features, and
-    k is the nearest neighbor setting for knn manifold estimation.
+    phi_a and phi_b are N_a x D and N_b x D sets of features,
+    k is the nearest neighbor setting for knn manifold estimation,
+    batch_size is the number of pairwise distance computations done at once (None = do them all at once)
     """
-    if parallel:
-        r_phi = torch.kthvalue(torch.cdist(phi_a, phi_a), k + 1, dim=0).values
+    if batch_size is None:
+        r_phi = torch.kthvalue(torch.cdist(phi_a, phi_a), k + 1, dim=1).values
         d_ba = torch.cdist(phi_b, phi_a)
-        out = torch.any(d_ba <= r_phi, dim=1)
+        b_in_a = torch.any(d_ba <= r_phi, dim=1).float()
     else:
         r_phi = torch.empty_like(phi_a[:, 0])
-        for i, pa in enumerate(phi_a):
-            d_aa = torch.cdist(pa.unsqueeze(dim=0), phi_a)
-            r_phi[i] = torch.kthvalue(d_aa, k + 1, dim=1).values
+        for batch_inds in torch.split(torch.arange(len(phi_a)), batch_size):
+            d_aa = torch.cdist(phi_a[batch_inds], phi_a)
+            r_phi[batch_inds] = torch.kthvalue(d_aa, k + 1, dim=1).values
+        b_in_a = torch.empty_like(phi_b[:, 0])
+        for batch_inds in torch.split(torch.arange(len(phi_b)), batch_size):
+            d_ba = torch.cdist(phi_b[batch_inds], phi_a)
+            b_in_a[batch_inds] = torch.any(d_ba <= r_phi, dim=1).float()
+    return b_in_a.mean().item()
 
-        out = torch.empty_like(phi_b[:, 0])
-        for j, pb in enumerate(phi_b):
-            d_ab = torch.cdist(pb.unsqueeze(dim=0), phi_a)
-            out[j] = torch.any(d_ab <= r_phi)
-    return out.float().mean().item()
 
-
-def precision_recall(phi_r, phi_g, k=3, parallel=True):
+def precision_recall(phi_r, phi_g, k=3, batch_size=None):
     """
     phi_r and phi_s are N_a x D and N_b x D sets of features
     from real and generated images respectively, and k is the nearest
     neighbor setting for knn manifold estimation.
     """
     return {
-        "precision": manifold_estimate(phi_r, phi_g, k=k, parallel=parallel),
-        "recall": manifold_estimate(phi_g, phi_r, k=k, parallel=parallel),
+        "precision": manifold_estimate(phi_r, phi_g, k=k, batch_size=batch_size),
+        "recall": manifold_estimate(phi_g, phi_r, k=k, batch_size=batch_size),
     }
