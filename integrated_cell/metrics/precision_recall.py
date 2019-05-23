a3c9415db97dc3b0f50a08b43e4ba86ba0756c31
Gregory Johnson <gregjohnso@gmail.com>
2019-05-23 09:32:06 -0700

    compatability for pytorch 1.0.0
    Former-commit-id: 5d241b8e5ed2c6e37ab9923a44dc0dce7c147935

--- integrated_cell/metrics/precision_recall.py
+++ integrated_cell/metrics/precision_recall.py
@@ -6,6 +6,16 @@ https://arxiv.org/abs/1904.06991
 """
 
 
+def my_cdist(x1, x2):
+    x1_norm = x1.pow(2).sum(dim=-1, keepdim=True)
+    x2_norm = x2.pow(2).sum(dim=-1, keepdim=True)
+    res = torch.addmm(
+        x2_norm.transpose(-2, -1), x1, x2.transpose(-2, -1), alpha=-2
+    ).add_(x1_norm)
+    res = res.clamp_min_(1e-30).sqrt_()
+    return res
+
+
 def manifold_estimate(phi_a, phi_b, k=3, batch_size=None):
     """
     phi_a and phi_b are N_a x D and N_b x D sets of features,
@@ -13,17 +23,17 @@ def manifold_estimate(phi_a, phi_b, k=3, batch_size=None):
     batch_size is the number of pairwise distance computations done at once (None = do them all at once)
     """
     if batch_size is None:
-        r_phi = torch.kthvalue(torch.cdist(phi_a, phi_a), k + 1, dim=1).values
-        d_ba = torch.cdist(phi_b, phi_a)
+        r_phi = torch.kthvalue(my_cdist(phi_a, phi_a), k + 1, dim=1)[0]
+        d_ba = my_cdist(phi_b, phi_a)
         b_in_a = torch.any(d_ba <= r_phi, dim=1).float()
     else:
         r_phi = torch.empty_like(phi_a[:, 0])
         for batch_inds in torch.split(torch.arange(len(phi_a)), batch_size):
-            d_aa = torch.cdist(phi_a[batch_inds], phi_a)
-            r_phi[batch_inds] = torch.kthvalue(d_aa, k + 1, dim=1).values
+            d_aa = my_cdist(phi_a[batch_inds], phi_a)
+            r_phi[batch_inds] = torch.kthvalue(d_aa, k + 1, dim=1)[0]
         b_in_a = torch.empty_like(phi_b[:, 0])
         for batch_inds in torch.split(torch.arange(len(phi_b)), batch_size):
-            d_ba = torch.cdist(phi_b[batch_inds], phi_a)
+            d_ba = my_cdist(phi_b[batch_inds], phi_a)
             b_in_a[batch_inds] = torch.any(d_ba <= r_phi, dim=1).float()
     return b_in_a.mean().item()
 
