b2232a331935e5500108395237176786f092c8de
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2019-05-28 12:22:44 -0700

    multiple ks with only one dist mat calc
    Former-commit-id: aa209cab83dd0492dc8d8f390aa74b8b0adb1e82

--- integrated_cell/metrics/precision_recall.py
+++ integrated_cell/metrics/precision_recall.py
@@ -16,35 +16,45 @@ def my_cdist(x1, x2):
     return res
 
 
-def manifold_estimate(phi_a, phi_b, k=3, batch_size=None):
+def manifold_estimate(phi_a, phi_b, k=[3], batch_size=None):
     """
     phi_a and phi_b are N_a x D and N_b x D sets of features,
     k is the nearest neighbor setting for knn manifold estimation,
     batch_size is the number of pairwise distance computations done at once (None = do them all at once)
     """
     if batch_size is None:
-        r_phi = torch.kthvalue(my_cdist(phi_a, phi_a), k + 1, dim=1)[0]
+        r_phi = {kappa:torch.kthvalue(my_cdist(phi_a, phi_a), kappa + 1, dim=1)[0] for kappa in k}
         d_ba = my_cdist(phi_b, phi_a)
-        b_in_a = torch.any(d_ba <= r_phi, dim=1).float()
+        b_in_a = {kappa:torch.any(d_ba <= rp, dim=1).float() for kappa,rp in r_phi.items()}
     else:
-        r_phi = torch.empty_like(phi_a[:, 0])
+        r_phi = {kappa:torch.empty_like(phi_a[:, 0]) for kappa in k}
         for batch_inds in torch.split(torch.arange(len(phi_a)), batch_size):
             d_aa = my_cdist(phi_a[batch_inds], phi_a)
-            r_phi[batch_inds] = torch.kthvalue(d_aa, k + 1, dim=1)[0]
-        b_in_a = torch.empty_like(phi_b[:, 0])
+            for kappa in k:
+                r_phi[kappa][batch_inds] = torch.kthvalue(d_aa, kappa + 1, dim=1)[0]
+        b_in_a = {kappa:torch.empty_like(phi_b[:, 0]) for kappa in k}
         for batch_inds in torch.split(torch.arange(len(phi_b)), batch_size):
             d_ba = my_cdist(phi_b[batch_inds], phi_a)
-            b_in_a[batch_inds] = torch.any(d_ba <= r_phi, dim=1).float()
-    return b_in_a.mean().item()
+            for kappa in k:
+                b_in_a[kappa][batch_inds] = torch.any(d_ba <= r_phi[kappa], dim=1).float()
+    return {key:value.mean().item() for key, value in b_in_a.items()}
 
 
-def precision_recall(phi_r, phi_g, k=3, batch_size=None):
+def precision_recall(phi_r, phi_g, k=[3], batch_size=None):
     """
     phi_r and phi_s are N_a x D and N_b x D sets of features
-    from real and generated images respectively, and k is the nearest
-    neighbor setting for knn manifold estimation.
+    from real and generated images respectively,
+    k is a list of integers for the nearest neighbor setting for knn manifold estimation.
+    returns a dict {k_1:{'precision':float, 'recall':float}, k_2:{'precision':float, 'recall':float}, ...}
     """
-    return {
+    d = {
         "precision": manifold_estimate(phi_r, phi_g, k=k, batch_size=batch_size),
         "recall": manifold_estimate(phi_g, phi_r, k=k, batch_size=batch_size),
     }
+
+    result = {}
+    for k1, subdict in d.items():
+        for k2, v in subdict.items():
+            result.setdefault(k2, {})[k1] = v
+
+    return result
