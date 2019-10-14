665a616270afc4f67a2bd6c0a184fddf242adde7
Gregory Johnson <gregj@alleninstitute.org>
2019-10-14 11:46:39 -0700

    docker ignore, some tests

--- examples/3D_benchmark_tests/run_3D.sh
+++ examples/3D_benchmark_tests/run_3D.sh
@@ -4,12 +4,17 @@ trainer_type=$3
 batch_size=$4
 ndat=$5
 
+image_dir=../../data/
+csv_name=metadata.csv
+# image_dir=/raid/shared/ipp/scp_19_04_10/
+# csv_name=controls/data_plus_controls.csv
+
 ic_train_model \
 	--gpu_ids $gpu_ids \
 	--model_type ae \
 	--save_dir $save_dir \
 	--lr_enc 2E-4 --lr_dec 2E-4 \
-	--data_save_path data.pyt \
+	--data_save_path $save_dir/data.pyt \
 	--crit_recon torch.nn.MSELoss \
 	--kwargs_crit_recon '{"reduction": "sum"}' \
 	--network_name vaegan3D_cgan \
@@ -19,9 +24,9 @@ ic_train_model \
 	--kwargs_dec_optim '{"betas": [0.9, 0.999]}' \
 	--kwargs_model '{"beta": 1, "objective": "H", "save_state_iter": 1E9, "save_progress_iter": 1E9}' \
 	--train_module $trainer_type \
-	--imdir /raid/shared/ipp/scp_19_04_10/ \
+	--imdir $image_dir \
 	--dataProvider DataProvider \
-	--kwargs_dp '{"crop_to": [160,96, 64], "return2D": 0, "check_files": 1, "make_controls": 0, "csv_name": "controls/data_plus_controls.csv"}' \
+	--kwargs_dp '{"crop_to": [160,96, 64], "return2D": 0, "check_files": 0, "make_controls": 0, "csv_name": "'$csv_name'"}' \
 	--saveStateIter 1 --saveProgressIter 1 \
 	--channels_pt1 0 1 2 \
 	--batch_size $batch_size  \
